79517e96722aa222f06c597f219576802ac7c427
duttprem <premdutt083@gmail.com>
2023-04-01 10:34:55 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
+ import { Article,Brand,CTA,Feature,Navbar } from './components';
+import { Blog,Footer,Features,Header,Possibility,WhatGPT } from './container';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='gradient__bg'>
+      <Navbar />
+        <Header />
+
+      </div>
+      <Brand />
+        <WhatGPT />
+        <Features />
+        <Possibility />
+      <CTA />
+        <Blog />
+        <Footer />
+      
+      
     </div>
   );
 }
